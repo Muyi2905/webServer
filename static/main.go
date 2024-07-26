4f0d8ac09027caea3fbc51f6b4a85cabb3f51d5c
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-07-26 13:08:43 +0100

    fileServer

--- static/main.go
+++ static/main.go
@@ -7,5 +7,13 @@ import (
 )
 
 func main(){
+	fileServer:= http.FileServer(http.Dir("/static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandle)
+	http.HandleFunc("/form", formHandle)
+
+	if err:=http.ListenAndServe(":8080", nil);err!= nil {
+		log.Fatal(err)
+	}
 	
 }
