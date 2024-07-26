ad0015d0ed9501f665a82b9e52e2c9e60cd79231
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-07-26 13:49:33 +0100

    hello route

--- static/main.go
+++ static/main.go
@@ -6,11 +6,24 @@ import (
 	"log"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello"{
+		http.Error(w, "404 not found",  http.StatusAccepted)
+	}
+
+	if r.Method != "GET"{
+		http.Error(w, "Method is not supported", http.StatusAccepted)
+	
+
+fmt.Fprintf(w, "hello")
+	}
+}
+
 func main(){
 	fileServer:= http.FileServer(http.Dir("/static"))
 	http.Handle("/", fileServer)
-	http.HandleFunc("/hello", helloHandle)
-	http.HandleFunc("/form", formHandle)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
 
 	if err:=http.ListenAndServe(":8080", nil);err!= nil {
 		log.Fatal(err)
